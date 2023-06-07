71d9064f1d590893439d09eddd92e19e94582f82
Jimmy Zhang <jimmyzhang1@hotmail.com>
2023-06-07 13:56:31 -0700

    Create CreatePost and Home pages, and allow them to be exported

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from "./assets";
+
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
   return <h1 className="text-3x1 font-bold underline">Hello world!</h1>;
