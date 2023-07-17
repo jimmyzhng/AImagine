7b0047bd7e34cd8209b4c6fb1d927521654ccf22
Jimmy Zhang <jimmyzhang1@hotmail.com>
2023-07-17 11:44:28 -0700

    write the post route to create a post

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -9,4 +9,34 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// GET all posts
+router.route('/').get(async (req, res) => {
+
+});
+
+// CREATE a post
+router.route('/').post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: error });
+
+  }
+});
+
 export default router;
